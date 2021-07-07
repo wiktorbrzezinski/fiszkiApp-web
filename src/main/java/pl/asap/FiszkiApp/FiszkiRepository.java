9c4595d7317c4fb06e9c593eb0fce5c9f0ae5105
wiktorbrzezinski <wiktor2002w@gmail.com>
2021-07-07 17:55:07 +0200

    Function of adding fiszki

--- src/main/java/pl/asap/FiszkiApp/FiszkiRepository.java
+++ src/main/java/pl/asap/FiszkiApp/FiszkiRepository.java
@@ -3,5 +3,10 @@ package pl.asap.FiszkiApp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FiszkiRepository extends JpaRepository<Fiszki, Long> {
+
+    Optional<Fiszki> findById(int id);
+
 }
